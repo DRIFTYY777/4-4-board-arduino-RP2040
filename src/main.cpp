0b1f2a2eaef8fcd3645ce15b05341cb017ec9362
Driftyy_777 <dhimandeepak517@gmail.com>
2025-07-02 13:51:48 +0530

    Add NeoPixelConnect library support and refactor main.cpp for RGB LED functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,21 @@
 #include <Arduino.h>
 #include "mbed.h"
 #include "USBMouseKeyboard.h"
+#include <NeoPixelConnect.h>
 
-// Rotary Encoder
+using namespace mbed;
+using namespace rtos;
+
+#define RGB_PIN 13 // RGB LED pin
+#define LED_COUNT 16
+#define BRIGHTNESS 100 // Set brightness to maximum
+
+// Rotary Encoder and Button pin out
 #define RT_BTN 17
 #define ENC_A 15
 #define ENC_B 14
 
-// Keypad Matrix
+// Keypad Matrix pin out
 #define ROW1 0
 #define ROW2 1
 #define ROW3 2
@@ -18,33 +26,31 @@
 #define COL3 26
 #define COL4 22
 
-
 constexpr int rowPins[4] = {ROW4, ROW3, ROW2, ROW1};
 constexpr int colPins[4] = {COL4, COL3, COL2, COL1};
 
 // Keypad matrix layout
 char keymap[4][4] = {
-    { '1', '2', '3', 'A' },
-    { '4', '5', '6', 'B' },
-    { '7', '8', '9', 'C' },
-    { '*', '0', '#', 'D' }
-};
-
-
+    {'1', '2', '3', 'A'},
+    {'4', '5', '6', 'B'},
+    {'7', '8', '9', 'C'},
+    {'*', '0', '#', 'D'}};
 
 // Previous state of the rotary encoder
 int prevA = -1;
 int prevB = -1;
 
 /// @brief Rotary Encoder State
-enum ROTARY_STATE {
+enum ROTARY_STATE
+{
     SCROLLING = 0,
     VOLUME_CONTROL = 1,
 };
 ROTARY_STATE currentRotaryState = SCROLLING;
 
 /// Rotary Button State
-enum ROTARY_BTN_STATE {
+enum ROTARY_BTN_STATE
+{
     PRESSED = 0,
     RELEASED = 1,
     LONG_PRESSED = 2,
@@ -52,25 +58,35 @@ enum ROTARY_BTN_STATE {
 
 /// USB Mouse and Keyboard instance
 USBMouseKeyboard key_mouse;
-
+// NeoPixel instance
+NeoPixelConnect RGB(RGB_PIN, LED_COUNT, pio0, 0);
+// Thread for RGB animation
+Thread thread;
 
 // return the button is pressed, long pressed or released
-ROTARY_BTN_STATE checkRotaryButton() {
+ROTARY_BTN_STATE checkRotaryButton()
+{
 
     static unsigned long lastPressTime = 0;
     static bool longPressHandled = false;
     const bool currentState = digitalRead(RT_BTN);
 
-    if (currentState == LOW) { // Button pressed
-        if (lastPressTime == 0) {
+    if (currentState == LOW)
+    { // Button pressed
+        if (lastPressTime == 0)
+        {
             lastPressTime = millis();
             longPressHandled = false;
             return PRESSED;
-        } else if (millis() - lastPressTime > 1000 && !longPressHandled) {
+        }
+        else if (millis() - lastPressTime > 1000 && !longPressHandled)
+        {
             longPressHandled = true;
             return LONG_PRESSED;
         }
-    } else { // Button released
+    }
+    else
+    { // Button released
         lastPressTime = 0;
         longPressHandled = false;
         return RELEASED;
@@ -79,55 +95,73 @@ ROTARY_BTN_STATE checkRotaryButton() {
 }
 
 // return 2 value via pointer
-void EncoderValue(int *a, int *b) {
+void EncoderValue(int *a, int *b)
+{
     const int enc_a = digitalRead(ENC_A);
     const int enc_b = digitalRead(ENC_B);
     *a = 0;
     *b = 0;
-    if (enc_a != prevA) {
-        if (enc_b != enc_a) {
-            *a = enc_a;  // Clockwise
-        } else {
-            *b = enc_b;  // Counter-clockwise
+    if (enc_a != prevA)
+    {
+        if (enc_b != enc_a)
+        {
+            *a = enc_a; // Clockwise
+        }
+        else
+        {
+            *b = enc_b; // Counter-clockwise
         }
     }
     prevA = enc_a;
     prevB = enc_b;
 }
 
-void scrollMouse() {
+void scrollMouse()
+{
     int a, b;
     EncoderValue(&a, &b);
-    if (a) {
+    if (a)
+    {
         Serial.print("Mouse Scrolling: ");
         Serial.println(a);
         key_mouse.scroll(static_cast<int8_t>(a));
-    } else if (b) {
+    }
+    else if (b)
+    {
         Serial.print("Mouse Scrolling: ");
         Serial.println(-b);
         key_mouse.scroll(static_cast<int8_t>(-b));
     }
 }
 
-void volumeControl() {
+void volumeControl()
+{
     int a, b;
     EncoderValue(&a, &b);
-    if (a) {
+    if (a)
+    {
         Serial.print("Volume Up: ");
         Serial.println(a);
         key_mouse.media_control(KEY_VOLUME_UP);
-    } else if (b) {
+    }
+    else if (b)
+    {
         Serial.print("Volume Down: ");
         Serial.println(-b);
         key_mouse.media_control(KEY_VOLUME_DOWN);
     }
 }
 
-void checkKeypad() {
-    for (int r = 0; r < 4; r++) {
+// Check the keypad matrix for pressed keys
+void checkKeypad()
+{
+    for (int r = 0; r < 4; r++)
+    {
         digitalWrite(rowPins[r], LOW);
-        for (int c = 0; c < 4; c++) {
-            if (digitalRead(colPins[c]) == LOW) {
+        for (int c = 0; c < 4; c++)
+        {
+            if (digitalRead(colPins[c]) == LOW)
+            {
                 Serial.print("Keypad Pressed: ");
                 Serial.println(keymap[r][c]);
                 key_mouse.printf("%c", keymap[r][c]);
@@ -138,54 +172,104 @@ void checkKeypad() {
     }
 }
 
-
-void blinkLED(const bool state) {
+// Blink the built-in LED to indicate the current state
+void blinkLED(const bool state)
+{
     digitalWrite(LED_BUILTIN, state ? HIGH : LOW);
 }
 
+// cyan Pulse
+void cyanPulse()
+{
+    static uint8_t brightness = 0;
+    static int fadeDirection = 1;
 
-void setup() {
+    // Adjust brightness
+    if (brightness == 0)
+    {
+        fadeDirection = 1; // Start fading in
+    }
+    else if (brightness >= BRIGHTNESS)
+    {
+        fadeDirection = -1; // Start fading out
+    }
+    brightness += fadeDirection * 5; // Adjust speed of the pulse
+
+    // Set color to cyan
+    RGB.neoPixelFill(0, brightness, brightness);
+    RGB.neoPixelShow();
+
+    delay(50); // Adjust delay for speed of pulse
+}
+
+
+
+// create a thread for the NeoPixel animation
+void RGB_THREAD()
+{
+    while (true)
+    {
+        cyanPulse();
+    }
+}
+
+void setup()
+{
     Serial.begin(115200);
+    RGB.neoPixelInit(RGB_PIN, LED_COUNT);
+    RGB.setBrightness(BRIGHTNESS); // Set brightness to maximum
+    RGB.neoPixelClear();           // Clear the NeoPixel strip
 
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(ENC_A, INPUT);
     pinMode(ENC_B, INPUT);
     pinMode(RT_BTN, INPUT_PULLUP);
 
-    for (const int rowPin : rowPins) {
+    for (const int rowPin : rowPins)
+    {
         pinMode(rowPin, OUTPUT);
         digitalWrite(rowPin, HIGH);
     }
-    for (const int colPin : colPins) {
+    for (const int colPin : colPins)
+    {
         pinMode(colPin, INPUT_PULLUP);
     }
 
     prevA = digitalRead(ENC_A);
     prevB = digitalRead(ENC_B);
+
+    thread.start(RGB_THREAD); // Start the NeoPixel animation thread
 }
 
-
-void loop() {
+void loop()
+{
 
     ROTARY_BTN_STATE btnState = checkRotaryButton();
 
     // Handle rotary state switching
-    if (btnState == LONG_PRESSED) {
+    if (btnState == LONG_PRESSED)
+    {
         currentRotaryState = (currentRotaryState == SCROLLING) ? VOLUME_CONTROL : SCROLLING;
         // Blink LED to indicate state change
         blinkLED(currentRotaryState == VOLUME_CONTROL);
-    } else if (btnState == PRESSED) {
+    }
+    else if (btnState == PRESSED)
+    {
         // Short press for mute in volume mode
-        if (currentRotaryState == VOLUME_CONTROL) {
+        if (currentRotaryState == VOLUME_CONTROL)
+        {
             key_mouse.media_control(KEY_MUTE);
             Serial.println("Mute");
         }
     }
 
     // Handle rotary functions based on a current state
-    if (currentRotaryState == SCROLLING) {
+    if (currentRotaryState == SCROLLING)
+    {
         scrollMouse();
-    } else {
+    }
+    else
+    {
         volumeControl();
     }
 
